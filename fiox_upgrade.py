695340a6015fe58ad020ce148f0b2c6c08d1fe66
jsengenberger-vs <40175863+jsengenberger-vs@users.noreply.github.com>
2018-08-05 11:49:32 -0600

    .

--- fiox_upgrade.py
+++ fiox_upgrade.py
@@ -1,6 +1,8 @@
 import os
 import command
 
-print commands.getoutput("wget https://github.com/jsengenberger-vs/fiox/archive/master.zip")
-print commands.getoutput("unzip master.zip")
-print commands.getoutput("mv ./fiox-master/* .")
+#Won't work in python 3, but this works in 2
+os.system("wget https://github.com/jsengenberger-vs/fiox/archive/master.zip")
+os.system("unzip master.zip")
+os.system("mv ./fiox-master/* .")
+os.system("rm -rf ./fiox-master")
