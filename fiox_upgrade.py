4cd57885badbfb6b90d49ce6b25b659ad4a1d65a
John Sengenberger <johnsengenberger@Johns-MacBook-Pro.local>
2018-08-05 11:50:55 -0600

    .

--- fiox_upgrade.py
+++ fiox_upgrade.py
@@ -1,8 +1,8 @@
 import os
-import command
 
 #Won't work in python 3, but this works in 2
 os.system("wget https://github.com/jsengenberger-vs/fiox/archive/master.zip")
 os.system("unzip master.zip")
 os.system("mv ./fiox-master/* .")
+os.system("mv ./fiox-master/tools/* ./tools/")
 os.system("rm -rf ./fiox-master")
